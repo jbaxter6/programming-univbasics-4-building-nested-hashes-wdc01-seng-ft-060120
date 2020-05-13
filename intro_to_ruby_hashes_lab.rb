ba90ff8211e7190e3006f0edad0bee79cfa96c42
Johnnathan Baxter <jbaxter6@radford.edu>
2020-05-13 05:50:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -48,7 +48,8 @@ def monopoly_with_fourth_tier
       :railroads => {}
     }
     
-
+hash[:railroads] = {pieces: 4, rent_in_dollars: {}, names: {}}
+hash[:railroads][:rent_in_dollars] = {one_piece_owned: 25, two_pieces_owned: 50, three_pieces_owned: 100, four_pieces_owned:200}
 hash[:railroads][:names] = {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad: {}, shortline_railroad: {}}
 hash
 end
