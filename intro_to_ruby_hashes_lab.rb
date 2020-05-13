2d696e33872c182f736765d2c721b09d22e8ef8a
Johnnathan Baxter <jbaxter6@radford.edu>
2020-05-13 05:32:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,9 +18,8 @@ def monopoly_with_second_tier
       :railroads => {}
     }
     
-hash[:railroads] = {pieces: 4}
-hash[:railroads] = {:rent_in_dollars}
-hash[:railroads][:rent_in_dollars] => {}
+hash[:railroads] = {pieces: 4, rent_in_dollars: {}, names: {}}
+
 hash
 
 end
